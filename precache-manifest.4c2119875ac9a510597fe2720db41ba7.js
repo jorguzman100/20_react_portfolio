d7f8f03d31361f7a5740451c8605ca62b07e044c
Jorge Guzman <jorguzman100@gmail.com>
2020-08-27 17:28:14 +0300

    Updates

---
+++ precache-manifest.4c2119875ac9a510597fe2720db41ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d37d5d8f59b50e43f8b69505fd5a90",
+    "url": "/20_react_portfolio/index.html"
+  },
+  {
+    "revision": "17db8a53b65f03b3ed7f",
+    "url": "/20_react_portfolio/static/css/main.2cb31ad2.chunk.css"
+  },
+  {
+    "revision": "d4622533d34805b0e844",
+    "url": "/20_react_portfolio/static/js/2.6fb776ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/20_react_portfolio/static/js/2.6fb776ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17db8a53b65f03b3ed7f",
+    "url": "/20_react_portfolio/static/js/main.b4c5f9c5.chunk.js"
+  },
+  {
+    "revision": "59fc2b96a0ec1cf4966a",
+    "url": "/20_react_portfolio/static/js/runtime-main.7cfa00be.js"
+  }
+]);
